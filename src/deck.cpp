00d2b9e64ff712064692536a3b2b42d63c22a88a
therealsamyak <therealsamyak@gmail.com>
2023-11-16 13:57:49 -0800

    added deck functionality

--- src/deck.cpp
+++ src/deck.cpp
@@ -1,11 +1,47 @@
 #include "../header/deck.hpp"
 #include "../header/hand.hpp"
 
+using namespace std;
+
+// generates the deck
 void Deck::generateDeck(){
-    
+    for (int value = Two; value <= A; value++) {
+
+        CardValue value_actual = static_cast<CardValue>(value); 
+
+        for (int suit = Clubs; suit <= Hearts; suit++) {
+
+            CardSuit suit_actual = static_cast<CardSuit>(suit);
+            
+            Card* newCard = new Card(value_actual, suit_actual);
+            Deck::obtainCard(newCard);
+        
+        }
+    }
+
+    Hand::sortHand();
 }
 
-Deck::Deck(){...}
+Deck::Deck(){
+    Deck::generateDeck();
+}
 
-Card* Deck::distributeRandomCard(){...}
+Deck::~Deck() {
+    Hand::clearHand();
+    hand.clear();
+}
+
+Card* Deck::distributeRandomCard(){
+    return nullptr;
+}
+
+void Deck::obtainCard(Card* card) {
+    if (hand.size() < MAX_SIZE) {
+        hand.push_back(card);
+    }
+
+    Hand::sortHand();
+
+    return;
+}
 
