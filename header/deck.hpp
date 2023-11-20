ac882981f1228aaefd1f21691aacd9df8e100a61
jahooba <josh.uapena20002@gmail.com>
2023-11-20 10:48:57 -0800

    Fixed spacing to 4

--- header/deck.hpp
+++ header/deck.hpp
@@ -2,9 +2,9 @@
 #include "../header/hand.hpp"
 
 class Deck: public Hand{
-        private:
-                void generateDeck();
-        public:
-                Deck();
-                Card* distributeRandomCard();
+    private:
+        void generateDeck();
+    public:
+        Deck();
+        Card* distributeRandomCard();
 };
