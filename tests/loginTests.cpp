32263a6c36307f906aca118acdfcf248b5b4e2a8
therealsamyak <therealsamyak@gmail.com>
2023-11-20 13:29:22 -0800

    fixed compilation issues with login & loginTests

--- tests/loginTests.cpp
+++ tests/loginTests.cpp
@@ -7,17 +7,30 @@ using namespace std;
 
 TEST(LoginTests, RegisterUser)
 {
-    Login login = Login("test_userdata.txt");
-    login.registerUser("test_user", "test_password");
-    EXPECT_TRUE(login.authenticateUser("test_user", "test_password"));
+    Login login = Login("testUserdata.txt");
+
+    string testUser = "testUser";
+    string testPassword = "testPassword";
+
+    login.registerUser(testUser, testUser);
+    
+    EXPECT_TRUE(login.authenticateUser(testUser, testPassword));
 }
 
 
 TEST(LoginTests, AuthenticateUserFail) 
 {
-    Login login = Login("test_userdata.txt");
-    login.registerUser("test_user", "test_password");
-    EXPECT_FALSE(login.authenticateUser("nonexistent_user", "wrong_password"));
+    Login login = Login("testUserdata.txt");
+    
+    string testUser = "testUser";
+    string testPassword = "testPassword";
+
+    string failUser = "nonexistentUser";
+    string wrongPassword = "wrongPassword";
+
+    login.registerUser(testUser, testUser);
+
+    EXPECT_FALSE(login.authenticateUser(failUser, wrongPassword));
 }
 
 
