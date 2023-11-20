72a736649796325d71c1cb2c2529d786d971bb12
therealsamyak <therealsamyak@gmail.com>
2023-11-20 13:18:50 -0800

    fixed header formatting * added pragma once to all header files that didn't have it * made naming consistent * fixed compilation errors

--- header/poker.hpp
+++ header/poker.hpp
@@ -7,7 +7,7 @@
 class Poker{
 	private:
 		Deck deck;
-		unordered_map<Hand, int> SCORE_KEY;
+		unordered_map<Hand, int, HandHasher> SCORE_KEY;
 		vector<Player*> playerList;
 		double pot;
 		void generateScoreKey();
