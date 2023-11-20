24f67abedf3d33e6f56167e2c7867a2bddae14fa
therealsamyak <therealsamyak@gmail.com>
2023-11-20 15:28:05 -0800

    added pokertest template *actual tests coming later

---
+++ tests/pokerTests.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "../header/poker.hpp"
+#include "../header/pokerScoreKey.hpp"
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
