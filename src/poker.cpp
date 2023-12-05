b6806318612883af5b9d3358f70ebd8bfa37744e
jahooba <josh.uapena20002@gmail.com>
2023-12-04 22:37:37 -0800

    Modified poker.cpp and pokerTests.cpp
    *poker.cpp no longer uses switch statement due to variables not being able to declared/initialized inside
    **switch statement replaced with nested if statements
    **added a if statement to immediately return in case the user doesn't have enough balance, so logic in while loop could also be adjusted
    *pokerTests.cpp no longer needs the DiscardTest as the GameTest runs it
    **I left it in there in case discardRound() needs to be adjusted
    *The way that cards are discarded and obtained could be optimized with functions

--- src/poker.cpp
+++ src/poker.cpp
@@ -22,105 +22,146 @@ Poker::~Poker() {
 
 void Poker::Game_Start(){
     bool playAgain = true;
+    double balance = playerList.at(0)->getBalance()->getBalance();
+
+    if(balance<=0){
+        cout << "Sorry, you don't have enough credits to play :(" << endl;
+        return;
+    }
+    
     //While the player has enough money and wants to play or only one player left
-    while (playerList.at(0)->getBalance() > 0 && playAgain == true && playerList.size() > 1){
+    while (balance > 0 && playAgain == true && playerList.size() > 1){
         //Distribute cards from deck to each player
-        playerList.at(0)->getHand()->obtainCard(deck.distributeRandomCard());
-        playerList.at(0)->getHand()->obtainCard(deck.distributeRandomCard());
-        playerList.at(0)->getHand()->obtainCard(deck.distributeRandomCard());
-        playerList.at(0)->getHand()->obtainCard(deck.distributeRandomCard());
-        playerList.at(0)->getHand()->obtainCard(deck.distributeRandomCard());
+        for(int i=0; i<5; i++){
+            playerList.at(0)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
+            playerList.at(1)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
+        }
+        cout << playerList.at(0)->getPlayerHand()->viewHand() << endl;
 
         //Present Poker actions for each player
 
         //Discard and draw for each player
         discardRound();
+
         //Present Poker actions for each player
 
         //Reveal hands for each player and decide winner
+        cout << endl << playerList.at(0)->getPlayerHand()->viewHand() << endl;
+        cout << playerList.at(1)->getPlayerHand()->viewHand() << endl;
         
-        //Prompt user with menu
-        cout << "1. Play again\n2.Return to Menu\n>";
-        int choice=0;
-        while (!(cin >> choice) || choice<0 || choice>2){
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Enter valid integer amount (1-2)> ";
-        }
-        if (choice==2){
-            playAgain = false;
+        //If player has enough credits, prompt user with menu
+        if (balance>0){
+            cout << "1. Play again\n2.Return to Menu\n>";
+            int choice=0;
+            while (!(cin >> choice) || choice<0 || choice>2){
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Enter valid integer amount (1-2)> ";
+            }
+            if (choice==2){
+                playAgain = false;
+            }
         }
         
     }
 }
 
 void Poker::discardRound(){
-    cout << "\n\tDISCARD ROUND\nHow many cards will you discard?\n(0-3) >";
+    //Prompt player for amount of cards to discard
+    cout << "\n\tDISCARD ROUND\nHow many cards will you discard? (0-3)> ";
     int discardAmount=-1;
+    //Validate user input is an int between 0 and 3
     while (!(cin >> discardAmount) || discardAmount<0 || discardAmount>3){
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Enter valid integer amount (0-3)> ";
     }
 
-    int firstDiscardPlace=0, secondDiscardPlace=0, thirdDiscardPlace=0;
-    switch (discardAmount)
-    {
-    case 1:
-        cout << "| 1st card = 1 | 2nd card = 2 | 3rd card= 3 | 4th card = 4 | 5th card = 5 |\n";
+
+    if (discardAmount==1){
+        //Display guide for choosing card
+        cout << "\n| 1st card = 1 | 2nd card = 2 | 3rd card= 3 | 4th card = 4 | 5th card = 5 |\n\n";
         cout << "Select the card to discard (1-5)> ";
+        int firstDiscardPlace=0;
+        //Validate user input is an int between 1 and 5
         while (!(cin >> firstDiscardPlace) || firstDiscardPlace<1 || firstDiscardPlace>5){
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Enter valid integer amount (1-5)> ";
         }
-        break;
 
-    case 2:
-        cout << "| 1st card = 1 | 2nd card = 2 | 3rd card= 3 | 4th card = 4 | 5th card = 5 |\n";
+        //Assigns the card to discard
+        Card* cardToDiscard = playerList.at(0)->getPlayerHand()->getHand().at(firstDiscardPlace-1);
+
+        //Deck obtains the discarded card
+        deck.obtainCard(playerList.at(0)->getPlayerHand()->distributeCard(cardToDiscard->value, cardToDiscard->suit));
+
+        //Player receives random card from deck
+        playerList.at(0)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
+    }
+    else if (discardAmount==2){
+        cout << "\n| 1st card = 1 | 2nd card = 2 | 3rd card= 3 | 4th card = 4 | 5th card = 5 |\n\n";
         cout << "Select the first card to discard (1-5)> ";
+        int firstDiscardPlace=0;
         while (!(cin >> firstDiscardPlace) || firstDiscardPlace<1 || firstDiscardPlace>5){
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Enter valid integer amount (1-5)> ";
         }
+        Card* firstCardToDiscard = playerList.at(0)->getPlayerHand()->getHand().at(firstDiscardPlace-1);
 
         cout << "Select the second card to discard (1-5)> ";
+        int secondDiscardPlace=0;
         while (!(cin >> secondDiscardPlace) || secondDiscardPlace<1 || secondDiscardPlace>5 || secondDiscardPlace==firstDiscardPlace){
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Enter valid integer amount (1-5)> ";
         }
-        break;
+        Card* secondCardToDiscard = playerList.at(0)->getPlayerHand()->getHand().at(secondDiscardPlace-1);
 
-    case 3:
-        cout << "| 1st card = 1 | 2nd card = 2 | 3rd card= 3 | 4th card = 4 | 5th card = 5 |\n";
+        deck.obtainCard(playerList.at(0)->getPlayerHand()->distributeCard(firstCardToDiscard->value, firstCardToDiscard->suit));
+        deck.obtainCard(playerList.at(0)->getPlayerHand()->distributeCard(secondCardToDiscard->value, secondCardToDiscard->suit));
+
+        playerList.at(0)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
+        playerList.at(0)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
+    }
+    else if (discardAmount==3){
+        cout << "\n| 1st card = 1 | 2nd card = 2 | 3rd card= 3 | 4th card = 4 | 5th card = 5 |\n\n";
         cout << "Select the first card to discard (1-5)> ";
+        int firstDiscardPlace=0;
         while (!(cin >> firstDiscardPlace) || firstDiscardPlace<1 || firstDiscardPlace>5){
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Enter valid integer amount (1-5)> ";
         }
+        Card* firstCardToDiscard = playerList.at(0)->getPlayerHand()->getHand().at(firstDiscardPlace-1);
 
         cout << "Select the second card to discard (1-5)> ";
+        int secondDiscardPlace=0;
         while (!(cin >> secondDiscardPlace) || secondDiscardPlace<1 || secondDiscardPlace>5 || secondDiscardPlace==firstDiscardPlace){
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Enter valid integer amount (1-5)> ";
         }
+        Card* secondCardToDiscard = playerList.at(0)->getPlayerHand()->getHand().at(secondDiscardPlace-1);
 
         cout << "Select the third card to discard (1-5)> ";
+        int thirdDiscardPlace=0;
         while (!(cin >> thirdDiscardPlace) || thirdDiscardPlace<1 || thirdDiscardPlace>5 || thirdDiscardPlace==secondDiscardPlace || thirdDiscardPlace==firstDiscardPlace){
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Enter valid integer amount (1-5)> ";
         }
-        break;
-    
-    default:
-        break;
+        Card* thirdCardToDiscard = playerList.at(0)->getPlayerHand()->getHand().at(thirdDiscardPlace-1);
+
+        deck.obtainCard(playerList.at(0)->getPlayerHand()->distributeCard(firstCardToDiscard->value, firstCardToDiscard->suit));
+        deck.obtainCard(playerList.at(0)->getPlayerHand()->distributeCard(secondCardToDiscard->value, secondCardToDiscard->suit));
+        deck.obtainCard(playerList.at(0)->getPlayerHand()->distributeCard(thirdCardToDiscard->value, thirdCardToDiscard->suit));
+
+        playerList.at(0)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
+        playerList.at(0)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
+        playerList.at(0)->getPlayerHand()->obtainCard(deck.distributeRandomCard());
     }
-    
 }
 
 void Poker::payout(Player* player){}
--- tests/pokerTests.cpp
+++ tests/pokerTests.cpp
@@ -3,21 +3,34 @@
 
 using namespace std;
 
-TEST(PokerDiscardRoundTests, DiscardTest){
+TEST(PokerGameTests, GameTest){
+    vector<Player*> playerList;
+    Hand* handOne = new Hand();
+    playerList.push_back(new Player("PlayerOne", 1.5 , handOne));
+    Hand* handTwo = new Hand();
+    playerList.push_back(new Player("PlayerTwo", 200, handTwo));
+
+    Poker game(playerList);
+    EXPECT_NO_THROW(game.Game_Start());
+}
+
+TEST(PokerGameTests, GameWithNoCreditsTest){
+    vector<Player*> playerList;
+    Hand* handOne = new Hand();
+    playerList.push_back(new Player("PlayerOne", 0, handOne));
+    Hand* handTwo = new Hand();
+    playerList.push_back(new Player("PlayerTwo", 200, handTwo));
+
+    Poker game(playerList);
+    EXPECT_NO_THROW(game.Game_Start());
+}
+
+/*TEST(PokerDiscardRoundTests, DiscardTest){
     vector<Player*> playerList;
     playerList.push_back(new Player());
     playerList.push_back(new Player());
     Poker game(playerList);
     EXPECT_NO_THROW(game.discardRound());
-}
-
-/*TEST(PokerGameTests, GameTest){
-    vector<Player*> playerList;
-    playerList.push_back(new Player());
-    playerList.push_back(new Player());
-    playerList.at(0)->getBalance()->setBalance(1);
-    Poker game(playerList);
-    EXPECT_NO_THROW(game.Game_Start());
 }*/
 
 
