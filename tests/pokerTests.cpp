a6a515410d490cfe00a352393d4a987fd000ee22
therealsamyak <therealsamyak@gmail.com>
2023-12-05 11:47:22 -0800

    reverted filename changes

--- tests/pokerTests.cpp
+++ tests/pokerTests.cpp
@@ -3,27 +3,27 @@
 
 using namespace std;
 
-TEST(PokerGameTests, GameTest){
-    vector<Player*> playerList;
-    Hand* handOne = new Hand();
-    playerList.push_back(new Player("PlayerOne", 1.5 , handOne));
-    Hand* handTwo = new Hand();
-    playerList.push_back(new Player("PlayerTwo", 200, handTwo));
+// TEST(PokerGameTests, GameTest){
+//     vector<Player*> playerList;
+//     Hand* handOne = new Hand();
+//     playerList.push_back(new Player("PlayerOne", 1.5 , handOne));
+//     Hand* handTwo = new Hand();
+//     playerList.push_back(new Player("PlayerTwo", 200, handTwo));
 
-    Poker game(playerList);
-    EXPECT_NO_THROW(game.Game_Start());
-}
+//     Poker game(playerList);
+//     EXPECT_NO_THROW(game.Game_Start());
+// }
 
-TEST(PokerGameTests, GameWithNoCreditsTest){
-    vector<Player*> playerList;
-    Hand* handOne = new Hand();
-    playerList.push_back(new Player("PlayerOne", 0, handOne));
-    Hand* handTwo = new Hand();
-    playerList.push_back(new Player("PlayerTwo", 200, handTwo));
+// TEST(PokerGameTests, GameWithNoCreditsTest){
+//     vector<Player*> playerList;
+//     Hand* handOne = new Hand();
+//     playerList.push_back(new Player("PlayerOne", 0, handOne));
+//     Hand* handTwo = new Hand();
+//     playerList.push_back(new Player("PlayerTwo", 200, handTwo));
 
-    Poker game(playerList);
-    EXPECT_NO_THROW(game.Game_Start());
-}
+//     Poker game(playerList);
+//     EXPECT_NO_THROW(game.Game_Start());
+// }
 
 /*TEST(PokerDiscardRoundTests, DiscardTest){
     vector<Player*> playerList;
